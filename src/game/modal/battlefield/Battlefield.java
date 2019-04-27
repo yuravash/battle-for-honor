e53915a7e60a89fac4a99a57a6314a564fe434a0
Yura Vashkiv <yuravash123@gmail.com>
2019-04-28 00:23:28 +0300

    Create cells(abstract class) and battlefiled(without realization)

---
+++ src/game/modal/battlefield/Battlefield.java
@@ -0,0 +1,5 @@
+package game.modal.battlefield;
+
+public class Battlefield{
+    /* TODO: create battlefield class */
+}
---
+++ src/game/modal/battlefield/cells/Cell.java
@@ -0,0 +1,21 @@
+package game.modal.battlefield.cells;
+
+
+public abstract class Cell {
+
+    private int posX;
+    private int posY;
+
+    public Cell(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+}
