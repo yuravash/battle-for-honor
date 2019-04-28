cc1cb843b3e3da9faf83db2d995b55282e4484d8
Yura Vashkiv <yuravash123@gmail.com>
2019-04-28 17:00:55 +0300

    Add docs

--- src/game/modal/entities/Copyable.java
+++ src/game/modal/entities/Copyable.java
@@ -1,5 +1,11 @@
 package game.modal.entities;
 
+/**
+ * Interface allows copying objects
+ * @author Yura Vashkiv
+ * @version 1.0
+ * @since 27.04.2019
+ */
 public interface Copyable{
     Copyable copy();
 }
